bc7294727e452bdcd649735a2de05a89fe22b87a
Duncan <duncan@vtllf.org>
2016-09-19 21:47:45 +1200

    Update documentation

---
+++ implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["xml"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='xml/struct.Parser.html' title='xml::Parser'>Parser</a>","impl&lt;'a,&nbsp;'b&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='xml/struct.ChildElements.html' title='xml::ChildElements'>ChildElements</a>&lt;'a,&nbsp;'b&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
